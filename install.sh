f000c66b216f7ac1d1d2449bb210917118ced405
MG Hacker <68454356+MangalNathYadav@users.noreply.github.com>
2022-03-26 10:00:34 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ apt-get update  # To get the latest package lists
 apt-get install nmap -y
 apt-get install gobuster -y
 apt-get install lolcat -y
+echo "now running the main script" 
+sed -i -e 's/\r$//' WebEnum.sh
+./WebEnum.sh
